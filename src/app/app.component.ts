b637181e8143aa3fa50ddd1c15ea9fe9bbf52733
Ahmedyasser11 <ahmedrashid816@yahoo.com>
2024-09-16 18:54:27 +0300

    view order Component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoginComponent } from './Components/login/login.component';
 
 import { privilegesComponent } from "./Components/privilege/privileges.component";
 import { FieldJobComponent } from './Components/Fieldjob/FieldJob.component';
+import { ViewOrderComponent } from './Components/view-order/view-order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, HttpClientModule, FieldJobComponent, privilegesComponent],
+  imports: [RouterOutlet, LoginComponent, HttpClientModule, FieldJobComponent, privilegesComponent,ViewOrderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
